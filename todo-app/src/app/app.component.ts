d59386ce2e78ba50aeca16b086c22a08f1459825
Rodolfo Dias Venancio <darth.rodolfo@gmail.com>
2021-02-24 14:43:58 -0300

    Adding more basic layout structure and beginning "viewModel code.

--- todo-app/src/app/app.component.ts
+++ todo-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-app';
+  public taskForm: FormGroup;
+  public Todos: [] = [];
+  public appState: AppStateList = AppStateList.HOME;
+
+
+  constructor(private formBuilder: FormBuilder) {
+    this.taskForm = this.formBuilder.group({
+      title:['333', Validators.compose([
+      Validators.maxLength(60),
+       Validators.minLength(3),
+        Validators.required,
+      ])]
+      });
+      //this.load();
+  }
+
+  changeAppState(newState: AppStateList) {
+    this.appState = newState;
+  }
+  addTask() {}
+  removeTask() {}
+  clearForm() {
+    this.taskForm.reset();
+  }
+  markAsDone() {}
+  markAsUndone() {}
+  saveTask() {}
+  load() {}
+
+
 }
+
+enum AppStateList {
+  HOME = 'home',
+  NOVA_TAREFA = 'nova_tarefa'
+}
+
