9ad0e60cc5763904b2a167137a951877608980a7
Rodolfo Dias Venancio <darth.rodolfo@gmail.com>
2021-02-24 15:47:33 -0300

    Adding functionality to the component/app

--- todo-app/src/app/app.component.ts
+++ todo-app/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToDoTask } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -9,11 +10,13 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class AppComponent {
   title = 'todo-app';
   public taskForm: FormGroup;
-  public Todos: [] = [];
-  public appState: AppStateList = AppStateList.HOME;
+  public toDosList: ToDoTask[] = [];
+  public appState: AppStateList = AppStateList.LISTA;
+  public appStateList = AppStateList;
 
 
   constructor(private formBuilder: FormBuilder) {
+
     this.taskForm = this.formBuilder.group({
       title:['333', Validators.compose([
       Validators.maxLength(60),
@@ -21,27 +24,64 @@ export class AppComponent {
         Validators.required,
       ])]
       });
-      //this.load();
+
+      this.loadToDoList();
   }
 
   changeAppState(newState: AppStateList) {
     this.appState = newState;
   }
-  addTask() {}
-  removeTask() {}
+
+  addTask() {
+    let title = this.taskForm.controls['title'].value;
+    let id = this.toDosList.length + 1;
+
+    this.toDosList.push(new ToDoTask(id,title, false));
+    this.saveTask();
+    this.clearForm();
+    this.changeAppState(AppStateList.LISTA);
+  }
+
+  removeTask(toDoTask: ToDoTask) {
+    let index = this.toDosList.indexOf(toDoTask);
+    if(index ===1) {
+      this.toDosList.splice(index, 1);
+    }
+    this.saveTask();
+  }
+
   clearForm() {
     this.taskForm.reset();
   }
-  markAsDone() {}
-  markAsUndone() {}
-  saveTask() {}
-  load() {}
 
+  markAsDone(toDoTask: ToDoTask) {
+    toDoTask.isDone = true;
+    this.saveTask();
+  }
+  markAsUndone(toDoTask: ToDoTask) {
+    toDoTask.isDone = false;
+    this.saveTask();
+  }
 
+  saveTask() {
+    const toDoListString = JSON.stringify(this.toDosList);
+    localStorage.setItem('toDoList', toDoListString);
+  }
+
+  loadToDoList() {
+    const savedToDoList = localStorage.getItem('toDoList');
+
+    if(savedToDoList == undefined) {
+       this.toDosList = [];
+       return;
+    }
+
+    this.toDosList = JSON.parse(savedToDoList);
+  }
 }
 
 enum AppStateList {
-  HOME = 'home',
+  LISTA = 'lista',
   NOVA_TAREFA = 'nova_tarefa'
 }
 
---
+++ todo-app/src/models/todo.model.ts
@@ -0,0 +1,6 @@
+export class ToDoTask {
+  constructor(public id: Number,
+     public title: String,
+      public isDone: Boolean) { }
+}
+
